3c205242a5a498bd77ce86eae2008b70de9f2b8b
Tanmoy Das <165408095+dasTanmoy0096@users.noreply.github.com>
2024-04-13 02:23:13 +0530

    finished project

---
+++ ShopIo/urls.py
@@ -0,0 +1,32 @@
+"""
+URL configuration for ShopIo project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic.base import TemplateView
+
+from core.views import frontpage, about
+
+urlpatterns = [
+	path('about/', about, name='about'),
+    path('admin/', admin.site.urls),
+	path('robots.txt', TemplateView.as_view(template_name='core/robots.txt', content_type="text/plain")),
+	path('', include('userprofile.urls')),
+	path('', include('store.urls')),
+	path('', frontpage, name='frontpage'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from store.models import Product
+
+# Create your views here.
+def frontpage(request):
+	products = Product.objects.filter(status=Product.ACTIVE)[0:6]
+
+	return render(request, 'core/frontpage.html', {
+		'products': products
+	})
+
+def about(request):
+	return render(request, 'core/about.html')
