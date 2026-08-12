562044c3f1ef765e04ddaec001a04407b00dfbe2
Tanmoy Das <dastanmoy0096@gmail.com>
2026-08-12 07:15:23 +0530

    Modularise WebApi Project, Create DataAccess Project, Setup NLog Logging

---
+++ WebApi/DataAccess/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace DataAccess;
+
+internal sealed class Class1 {
+
+}
--- WebApi/WebApi/Program.cs
+++ WebApi/WebApi/Program.cs
@@ -3,12 +3,19 @@ namespace WebApi;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Logging;
+
+using NLog.Web;
 
 internal sealed class Program {
     internal Program() { }
 
     internal static async Task Main() {
         WebApplicationBuilder builder = WebApplication.CreateBuilder();
+
+        builder.Logging.ClearProviders();
+        builder.Host.UseNLog();
+
         WebApplication app = builder.Build();
 
         app.MapGet("/", () => "Hello World!");
